726aa9ab5153fdba6ae51f6b60b31816f69099f7
Winston0323 <zil004@ucsd.edu>
2022-04-04 10:50:39 -0700

    add core, add object

---
+++ src/core.h
@@ -0,0 +1,11 @@
+#ifndef _CORE_H_
+#define _CORE_H_
+
+
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <vector>
+#include <ctype.h>
+#include <string>
+#include <iostream>
+#endif
--- src/main.h
+++ src/main.h
@@ -1,10 +1,8 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
-#include <glm/glm.hpp>
-#include <iostream>
-#include <string>
-#include "FreeImage.h"
 
+#include "core.h"
+#include "FreeImage.h"
 
 #endif
