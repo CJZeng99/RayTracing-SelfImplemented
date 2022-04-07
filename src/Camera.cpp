1be7fa5aac77f979709d49f512de524b6f5f647f
UCSDwezeng <wezeng@ucsd.edu>
2022-04-07 10:40:44 -0700

    refactor - Camera def

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,20 +1,44 @@
 #include "Camera.h"
 
-Camera::Camera() {}
+Camera::Camera(const glm::vec3& eye, const glm::vec3& center, const glm::vec3& up)
+    : eye(eye), center(center), up(up)
+{
+    float aspect = (float)w / (float)h;
+    float pix = w * h;
 
-Camera::~Camera() {}
+    // camera orientation initialization
+    Perspective(fovy, aspect, zNear, zFar);
+
+    // pixels initialization
+    pixels = new unsigned char[3 * pix];
+    for (int i = 0; i < 3 * pix; i++)
+        pixels[i] = '\0';
+}
+
+Camera::~Camera()
+{
+    // clear pixels
+    delete[] pixels;
+}
 
 void Camera::Perspective(float fovy, float aspect, float zNear, float zFar)
 {
-    
     float d = glm::tan(90.0f - fovy / 2.0f);
     float d_asp = d / aspect;
     float A = -(zFar + zNear) / (zFar - zNear);
     float B = -(2.0f * zFar * zNear) / (zFar - zNear);
-    projection = glm::mat4(
+    projectionMtx = glm::mat4(
         d_asp, 0.0f, 0.0f, 0.0f,
         0.0f, d, 0.0f, 0.0f,
         0.0f, 0.0f, A, -1.0f,
         0.0f, 0.0f, B, 0.0f
     );
+}
+
+void Camera::SaveScreenshot() {
+    FIBITMAP* img = FreeImage_ConvertFromRawBits(pixels, w, h, w * 3, 24, 0xFF0000, 0x00FF00, 0x0000FF, false);
+
+    std::cout << "Saving screenshot: " << outputFile << "\n";
+
+    FreeImage_Save(FIF_PNG, img, outputFile.c_str(), 0);
 }
--- src/Camera.h
+++ src/Camera.h
@@ -2,24 +2,35 @@
 #define _CAMERA_H_
 
 #include <glm/glm.hpp>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "FreeImage.h"
 
 class Camera
 {
 public:
-	Camera();
+	Camera(const glm::vec3& eye, const glm::vec3& center, const glm::vec3& up);
 	~Camera();
 
-	int w, h;
+	static int w, h;
+	static float fovy;
+	static float zNear;
+	static float zFar;
+	std::string outputFile;
 
 	glm::vec3 eye;
 	glm::vec3 center;
 	glm::vec3 up;
-	glm::mat4 projection;
-
-	unsigned char pixels[];
+	glm::mat4 projectionMtx;
+	unsigned char* pixels;
 
 	void Perspective(float fovy, float aspect, float zNear, float zFar);
+	void SaveScreenshot();
 };
 
+float Camera::zNear = 0.1f;
+float Camera::zFar = 99.0f;
 #endif
 
