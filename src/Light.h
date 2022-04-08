f5357430ea685a7a3c199bd0b34b719065024522
UCSDwezeng <wezeng@ucsd.edu>
2022-04-07 17:10:44 -0700

    feat - Light class

--- src/Light.h
+++ src/Light.h
@@ -1,6 +1,27 @@
 #pragma once
+#include <glm/glm.hpp>
+
+enum class LightType{directional, point};
+
 class Light
 {
+public:
+	LightType type;
+	glm::vec3 ambient = glm::vec3(0.2f, 0.2f, 0.2f);
+	float attenuation[3] = { 1.0f, 0.0f, 0.0f };
+};
 
+class DirectionalLight : public Light
+{
+public:
+	glm::vec3 direction;
+	DirectionalLight(const glm::vec3& direction, const glm::vec3& );
+};
+
+class PointLight : public Light
+{
+public:
+	glm::vec3 position;
+	PointLight(const glm::vec3& position);
 };
 
