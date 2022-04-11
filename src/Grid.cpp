604c182f659d9eda061d39f05e461a1a0ad38678
Winston0323 <zil004@ucsd.edu>
2022-04-11 10:47:11 -0700

    add ray box intersection test

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -2,5 +2,53 @@
 
 bool Grid::checkIntersect(Ray* ray, bool checkShadow = true)
 {
+	//ray property
+	float Ox = ray->getOrigin().x;
+	float Oy = ray->getOrigin().y;
+	float Oz = ray->getOrigin().z;
+	float Dx = ray->getDirection().x;
+	float Dy = ray->getDirection().y;
+	float Dz = ray->getDirection().z;
+	
+	//box property
+	float minX = min.x;
+	float minY = min.y;
+	float minZ = min.z;
+	float maxX = max.x;
+	float maxY = max.y;
+	float maxZ = max.z;
 
+	float tXMin = (minX - Ox) / Dx;
+	float tXMax = (maxX - Ox) / Dx;
+	if (tXMin > tXMax) {
+		std::swap(tXMin, tXMax);
+	}
+
+	float tYMin = (minY - Oy) / Dy;
+	float tYMax = (maxY - Oy) / Dy;
+	if (tYMin > tYMax) {
+		std::swap(tYMin, tYMax);
+	}
+
+	if ((tXMin > tYMax) || (tYMin > tXMax)) {
+		return false;
+	}
+		
+	if (tYMin > tXMin) {
+		tXMin = tYMin;
+	}
+	if (tYMax < tXMax) {
+		tXMax = tYMax;
+	}
+
+	float tZMin = (minZ - Oz) / Dz;
+	float tZMax = (maxZ - Oz) / Dz;
+	if (tZMin > tZMax) {
+		std::swap(tZMin, tZMax);
+	}
+	if ((tXMin > tZMin) || (tZMin > tXMax)) {
+		return false;
+	}
+
+	return true;
 }
